f20c479cce3fc01d46dfd830c0001f89fbfda708
buckethead1986 <rspeckpfs@gmail.com>
2017-09-30 13:31:19 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
 # Enter your procedural solution here!
+require 'pry'
+  def collect_multiples(limit)
+    # binding.pry
+    array = (1..limit-1)
+    array2 = array.select {|x| x%3 == 0 || x%5 ==0}
+    # array2.inject(:+)
+  end
+
+  def sum_multiples(limit)
+    array = (1..limit-1)
+    array2 = array.select {|x| x%3 == 0 || x%5 ==0}
+    array2.inject(:+)
+  end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    # binding.pry
+    array = (1..limit-1)
+    array2 = array.select {|x| x%3 == 0 || x%5 ==0}
+    # array2.inject(:+)
+  end
+
+  def sum_multiples
+    array = (1..limit-1)
+    array2 = array.select {|x| x%3 == 0 || x%5 ==0}
+    array2.inject(:+)
+  end
+
+
+end
